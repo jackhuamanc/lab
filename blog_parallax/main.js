56c96141f2cb12ccc22892b62e4522e59d421057
Jack Huaman <Jack@MacBook-Pro-de-Jack.local>
2016-11-30 09:05:39 -0500

    proyecto parallax

---
+++ blog_parallax/main.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+  parallax ();
+
+});
+var parallax = function () {
+  $(window).scroll(function(){
+		var barra = $(window).scrollTop();
+		var posicion =  (barra * 0.10);
+
+		$('.banner').css({
+			'background-position': '0 -' + posicion + 'px'
+		});
+
+	});
+}
