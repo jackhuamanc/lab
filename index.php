1434249d7d149d47e49d31196fd518da19a51653
Jack Huaman <Jack@MacBook-Pro-de-Jack.local>
2016-11-30 17:05:11 -0500

    php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
 <body>
   <section>
-        <div class="logo"><h1>Jack lab</h1></div>
+        <div class="logo"><h1>Jack lab X</h1></div>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.‹›</p>
 
